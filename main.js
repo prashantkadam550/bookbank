9eba3af187208fa7839d32c5efa960132de98980
Prashant Kadam <prashantkadam550@gmail.com>
2019-04-22 18:30:46 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+let navbar = $(".navbar");
+$(window).scroll(function () {
+    let oTop = $(".section-2").offset().top - window.innerHeight;
+    if ($(window).scrollTop() > oTop) {
+        navbar.addClass("sticky");
+    } else {
+        navbar.removeClass("sticky");
+    }
+})
